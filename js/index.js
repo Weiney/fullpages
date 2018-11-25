ef7c7dc91e3d99f7993ddeba7cc2a802672f0084
Weiney <admin@weiney.com>
2018-11-25 16:17:47 +0800

    项目提交
    项目提交

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        //options here
+        licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+        // verticalCentered:false,
+
+        // sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'],
+        navigation: true,
+        scrollingSpeed: 500, //滚动速度
+
+        easingcss3: "cubic-bezier(0.680, 0.055, 0.450, 0.940)",
+
+        scrollOverflow: true,//定义超出页长的section添加滚动条
+
+    });
+    console.log("CopyRight:Weiney Email:admin@weiney.com;Blog:www.weiney.com");
+    //methods
+});
+
+
